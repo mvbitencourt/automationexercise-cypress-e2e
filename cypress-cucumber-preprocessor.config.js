d01e45df73c4380a51f42cea3add4ac87108b603
Marcus Bitencourt <marcus.bitencourt@cepedi.org.br>
2025-12-26 03:53:43 -0300

    QA-23 T04.1 – Correçao de referencia

--- cypress-cucumber-preprocessor.config.js
+++ cypress-cucumber-preprocessor.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@badeball/cypress-cucumber-preprocessor");
 
 module.exports = defineConfig({
-  stepDefinitions: ["cypress/support/step_definitions/**/*.js"],
+  stepDefinitions: ["cypress/support/stepDefinitions/**/*.js"],
 });
