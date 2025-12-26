60cf68e3f22bf6ab971b8dcbb9aed096b546c10d
Marcus Bitencourt <marcus.bitencourt@cepedi.org.br>
2025-12-26 00:31:52 -0300

    QA-21 T03.3 – Implementar LoginPage (Page Object) Correções

--- cypress/support/pageObjects/loginPage.js
+++ cypress/support/pageObjects/loginPage.js
@@ -8,35 +8,10 @@ class LoginPage {
             passwordLoginInput: () => cy.get("//input[@data-qa='login-password']"),
             nameRegisterInput: () => cy.get("//input[@placeholder='Name']"),
             emailRegisterInput: () => cy.get("//input[@data-qa='signup-email']"),
-            titleRegisterInput: () => cy.get("//input[@id='id_gender1']"),
-            passwordRegisterInput: () => cy.get("//input[@id='password']"),
-            firstNameRegisterInput: () => cy.get("//input[@id='first_name']"),
-            lastNameRegisterInput: () => cy.get("//input[@id='last_name']"),
-            companyRegisterInput: () => cy.get("//input[@id='company']"),
-            addressRegisterInput: () => cy.get("//input[@id='address1']"),
-            address2RegisterInput: () => cy.get("//input[@id='address2']"),
-            stateRegisterInput: () => cy.get("//input[@id='state']"),
-            cityRegisterInput: () => cy.get("//input[@id='city']"),
-            zipCodeRegisterInput: () => cy.get("//input[@id='zipcode']"),
-            mobileNumberRegisterInput: () => cy.get("//input[@id='mobile_number']"),
         },
         buttons: {
             loginButton: () => cy.get("//button[normalize-space()='Login']"),
             registerButton: () => cy.get("//button[normalize-space()='Signup']"),
-            createAccountButton: () => cy.get("//button[normalize-space()='Create Account']"),
-            continueButton: () => cy.get("//a[normalize-space()='Continue']"),
-            logoutButton: () => cy.get("//a[normalize-space()='Logout']"),
-            deleteAccountButton: () => cy.get("//a[normalize-space()='Delete Account']"),
-        },
-        selects: {
-            dayRegisterSelect: () => cy.get("//select[@id='days']"),
-            monthRegisterSelect: () => cy.get("//select[@id='months']"),
-            yearRegisterSelect: () => cy.get("//select[@id='years']"),
-            countryRegisterSelect: () => cy.get("//select[@id='country']"),
-        },
-        checkboxes: {
-            newsletterCheckbox: () => cy.get("//input[@id='newsletter']"),
-            specialOffersCheckbox: () => cy.get("//input[@id='optin']"),
         },
         pages: {
             loginRegisterPage: () => cy.get("//h2[normalize-space()='Login to your account']"),
@@ -44,7 +19,6 @@ class LoginPage {
         messages: {
             successLoginOrRegister: () => cy.get("//li[10]//a[1]"),
             errorLoginEmailOrPasswordIncorrect: () => cy.get("//p[normalize-space()='Your email or password is incorrect!']"),
-
         },
     };
 
